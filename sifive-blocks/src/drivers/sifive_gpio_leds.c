7b22882f72cad879b22c85f7614ca0aae0a8477f
Keith Packard <keithp@keithp.com>
2020-07-15 16:24:42 -0700

    Define CPP symbols for LEDs, rather than using a lookup function
    This allows applications to know at build time whether the LED they
    want is available, rather than having to wait until run time.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- sifive-blocks/src/drivers/sifive_gpio_leds.c
+++ sifive-blocks/src/drivers/sifive_gpio_leds.c
@@ -12,40 +12,6 @@
 #include <stdbool.h>
 #include <string.h>
 
-struct metal_led sifive_gpio_leds_get(char *label) {
-    assert(label != NULL);
-    for(uint32_t i = 0; i < __METAL_DT_NUM_SIFIVE_GPIO_LEDS; i++) {
-        struct metal_led led = (struct metal_led) { i };
-        if(strcmp(LED_LABEL(led), label) == 0) {
-            return led;
-        }
-    }
-    assert(false);
-}
-
-struct metal_led sifive_gpio_leds_get_rgb(char *label, char *color) {
-    assert(label != NULL);
-    assert(color != NULL);
-
-    char led_label[100];
-    strncpy(led_label, label, sizeof(led_label));
-    led_label[sizeof(led_label) - 1] = '\0';
-
-    /* Append the color to the buffer */
-    size_t label_length = strlen(led_label);
-    if (sizeof(led_label) > label_length) {
-        strncat(led_label, color, sizeof(led_label) - label_length);
-    }
-
-    for(uint32_t i = 0; i < __METAL_DT_NUM_SIFIVE_GPIO_LEDS; i++) {
-        struct metal_led led = (struct metal_led) { i };
-        if(strcmp(LED_LABEL(led), led_label) == 0) {
-            return led;
-        }
-    }
-    assert(false);
-}
-
 void sifive_gpio_leds_enable(struct metal_led led) {
     metal_gpio_disable_input(LED_GPIO(led), LED_GPIO_PIN(led));
     metal_gpio_enable_output(LED_GPIO(led), LED_GPIO_PIN(led));
